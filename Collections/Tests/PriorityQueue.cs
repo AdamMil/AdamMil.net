a034794392c33d5a8a06eb57fe03233dbfda5567
adam <adam@442327cf-642b-0410-b943-f772b973994e>
2007-10-02 07:00:15 +0000

    Fixed a subtle bug in Enqueue() that was exposed by the recent Dequeue() change. Made PriorityQueue<T> maintain its own array rather than using List<T>. Created a new enumerator for the PriorityQueue that returns the items in sorted order.

--- Collections/Tests/PriorityQueue.cs
+++ Collections/Tests/PriorityQueue.cs
@@ -13,8 +13,8 @@ public class PriorityQueueTest
   [Test]
   public void Test()
   {
-    // actually, these numbers aren't quite random. they were tweaked to exercise all paths within HeapifyNode()
-    int[] randomNumbers = new int[] { 8, 4, 0, 2, 5, 7, 1, 8, 6, 3, 8, 9 };
+    // these numbers were tweaked to exercise all paths within HeapifyNode()
+    int[] numbers = new int[] { 8, 4, 0, 2, 5, 7, 1, 8, 6, 3, 8, 9 };
 
     PriorityQueue<int> queue;
 
@@ -27,32 +27,32 @@ public class PriorityQueueTest
     TestHelpers.TestException<InvalidOperationException>(delegate() { queue.Dequeue(); });
     TestHelpers.TestException<InvalidOperationException>(delegate() { queue.Peek(); });
 
-    List<int> list = new List<int>(randomNumbers);
-    AddItems(queue, randomNumbers);
+    List<int> list = new List<int>(numbers);
+    AddItems(queue, numbers);
     Assert.AreEqual(queue.Peek(), 9);
     SortedDequeue(queue, list);
 
-    AddItems(queue, randomNumbers);
+    AddItems(queue, numbers);
 
     queue.Capacity = 20;
     Assert.AreEqual(20, queue.Capacity);
 
     queue.TrimExcess();
-    Assert.AreEqual(randomNumbers.Length, queue.Capacity);
+    Assert.AreEqual(numbers.Length, queue.Capacity);
 
-    foreach(int i in randomNumbers) Assert.IsTrue(queue.Contains(i));
+    foreach(int i in numbers) Assert.IsTrue(queue.Contains(i));
     Assert.IsFalse(queue.Contains(20));
 
-    list = new List<int>(randomNumbers);
+    list = new List<int>(numbers);
     TestRemove(queue, list, 7);
 
-    list = new List<int>(randomNumbers);
+    list = new List<int>(numbers);
     TestRemove(queue, list, 4, 1, 8, 8);
 
-    list = new List<int>(randomNumbers);
+    list = new List<int>(numbers);
     TestRemove(queue, list, 8, 8, 4, 6);
 
-    Assert.IsFalse(queue.Remove(20));
+    Assert.IsFalse(((ICollection<int>)queue).Remove(20));
 
     queue.Clear();
     Assert.AreEqual(0, queue.Count);
@@ -70,6 +70,17 @@ public class PriorityQueueTest
     Assert.AreEqual(items.Count, queue.Count);
     items.Sort();
     items.Reverse(); // the queue will return items in reverse order (highest to lowest)
+
+    // first test using the enumerator
+    int index = 0;
+    foreach(int i in queue)
+    {
+      Assert.IsTrue(index < items.Count);
+      Assert.AreEqual(items[index], i);
+      index++;
+    }
+
+    // then actually dequeue the items
     foreach(int i in items) Assert.AreEqual(i, queue.Dequeue());
     Assert.AreEqual(queue.Count, 0);
   }
@@ -81,7 +92,7 @@ public class PriorityQueueTest
     foreach(int i in toRemove)
     {
       items.Remove(i);
-      queue.Remove(i);
+      ((ICollection<int>)queue).Remove(i);
     }
     SortedDequeue(queue, items);
   }
