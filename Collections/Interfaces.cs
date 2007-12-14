5b111cc7dfa036800843095c1692afb6b95ff22b
adam <adam@442327cf-642b-0410-b943-f772b973994e>
2007-12-14 21:06:10 +0000

    Collections\Interfaces.cs: + Made IQueue<T> inherit from ICollection<T>

--- Collections/Interfaces.cs
+++ Collections/Interfaces.cs
@@ -4,10 +4,14 @@ using System.Collections.Generic;
 namespace AdamMil.Collections
 {
 
-interface IQueue<T> : IEnumerable<T>
+/// <summary>An abstract interface for a queue of items.</summary>
+public interface IQueue<T> : ICollection<T>
 {
+  /// <summary>Adds an item to the queue.</summary>
   void Enqueue(T item);
+  /// <summary>Returns and removes the first item from the queue.</summary>
   T Dequeue();
+  /// <summary>Returns the first item in the queue.</summary>
   T Peek();
 }
 
