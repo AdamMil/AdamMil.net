e6d5fc37572fdce16e29628a74fc37bef7634d68
adam <adam@442327cf-642b-0410-b943-f772b973994e>
2008-06-29 19:52:19 +0000

    IO\Tests\IO.Tests.csproj: IO\Tests\Pipes.cs: + Added some basic pipe tests

---
+++ IO/Tests/Pipes.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using Microsoft.Win32.SafeHandles;
+using NUnit.Framework;
+using AdamMil.IO;
+using AdamMil.Tests;
+
+namespace AdamMil.IO.Tests
+{
+
+[TestFixture]
+public class PipeTest
+{
+  [Test]
+  public void Test()
+  {
+    using(InheritablePipe pipe = new InheritablePipe())
+    {
+      FileStream server = new FileStream(new SafeFileHandle(pipe.ServerHandle, false), FileAccess.ReadWrite);
+      FileStream client = new FileStream(new SafeFileHandle(pipe.ClientHandle, false), FileAccess.ReadWrite);
+
+      byte[] writeBuffer = System.Text.Encoding.UTF8.GetBytes("Hello, world!");
+      byte[] readBuffer = new byte[writeBuffer.Length];
+
+      client.Write(writeBuffer, 0, writeBuffer.Length);
+      client.Flush();
+
+      server.Write(writeBuffer, 0, writeBuffer.Length);
+      server.Flush();
+
+      int index = 0, count = readBuffer.Length;
+      while(count != 0)
+      {
+        int read = server.Read(readBuffer, index, count);
+        if(read == 0) break;
+        index += read;
+        count -= read;
+      }
+
+      Assert.AreEqual(index, readBuffer.Length);
+      CollectionHelpers.ArrayEquals(writeBuffer, readBuffer);
+
+
+      Array.Clear(readBuffer, 0, readBuffer.Length);
+      index = 0;
+      count = readBuffer.Length;
+      while(count != 0)
+      {
+        int read = client.Read(readBuffer, index, count);
+        if(read == 0) break;
+        index += read;
+        count -= read;
+      }
+
+      Assert.AreEqual(index, readBuffer.Length);
+      CollectionHelpers.ArrayEquals(writeBuffer, readBuffer);
+    }
+  }
+}
+
+} // namespace AdamMil.IO.Tests
