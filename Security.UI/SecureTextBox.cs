27f67d89e5b2330731906d1df9c0a5fc19def122
Adam Milazzo <adam@adammil.net>
2011-06-15 22:16:15 +0000

    Security.UI\SecureTextBox.cs: * Added support for clipboard operations (previously, they could cause crashes)

--- Security.UI/SecureTextBox.cs
+++ Security.UI/SecureTextBox.cs
@@ -22,6 +22,7 @@ using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Windows.Forms;
+using System.Text;
 
 namespace AdamMil.Security.UI
 {
@@ -59,6 +60,27 @@ public class SecureTextBox : TextBox
     set { throw new NotSupportedException(); }
   }
 
+  /// <summary>Removes all text from the <see cref="SecureTextBox"/>. This method should be used rather than the method on the
+  /// base class (<see cref="TextBox"/>).
+  /// </summary>
+  public new void Clear()
+  {
+    text.Clear();
+    base.Clear();
+  }
+
+  /// <summary>Throws a <see cref="NotSupportedException"/>. Do not call the method on the base class (<see cref="TextBox"/>).</summary>
+  public new void Copy()
+  {
+    throw new NotSupportedException();
+  }
+
+  /// <summary>Throws a <see cref="NotSupportedException"/>. Do not call the method on the base class (<see cref="TextBox"/>).</summary>
+  public new void Cut()
+  {
+    throw new NotSupportedException();
+  }
+
   /// <summary>Returns an estimate of the text strength, assuming it's a password entered by a human.</summary>
   /// <seealso cref="PGPUI.GetPasswordStrength"/>
   public PasswordStrength GetPasswordStrength()
@@ -78,6 +100,41 @@ public class SecureTextBox : TextBox
   public SecureString GetText()
   {
     return text.Copy();
+  }
+
+  /// <summary>Inserts the clipboard contents into the password, overwriting the current selection if there is one. Do not call
+  /// the method on the base class (<see cref="TextBox"/>).
+  /// </summary>
+  public new void Paste()
+  {
+    if(Clipboard.ContainsText())
+    {
+      string text = Clipboard.GetText();
+      if(!string.IsNullOrEmpty(text))
+      {
+        Paste(text);
+        text = null;
+        GC.Collect(); // try to remove the text from memory
+      }
+    }
+  }
+
+  /// <summary>Inserts the given text into the password, overwriting the current selection if there is one. Do not call the
+  /// method on the base class (<see cref="TextBox"/>).
+  /// </summary>
+  public new void Paste(string text)
+  {
+    if(SelectionLength != 0) DeleteSelection(true);
+    if(text != null)
+    {
+      int start = Math.Min(text.Length, SelectionStart);
+      for(int i=text.Length-1; i >= 0; i--)
+      {
+        char c = text[i];
+        if(!IsCharRestricted(c) && c >= 32 && !char.IsControl(c)) this.text.InsertAt(SelectionStart, c);
+      }
+      base.Text = base.Text.Insert(SelectionStart, new string('*', text.Length));
+    }
   }
 
   /// <summary>Sets the text to a copy of the given <see cref="SecureString"/>.</summary>
@@ -120,12 +177,12 @@ public class SecureTextBox : TextBox
     }
     else if(e.KeyCode == Keys.Delete)
     {
-      if(SelectionLength != 0) DeleteSelection(); // delete the selection if there is one
+      if(SelectionLength != 0) DeleteSelection(false); // delete the selection if there is one
       else if(SelectionStart < TextLength) text.RemoveAt(SelectionStart); // otherwise, delete the character at the cursor
     }
     else if(e.KeyCode == Keys.Back)
     {
-      if(SelectionLength != 0) DeleteSelection(); // delete the selection if there is one
+      if(SelectionLength != 0) DeleteSelection(false); // delete the selection if there is one
       else if(SelectionStart != 0) text.RemoveAt(SelectionStart-1); // otherwise, delete the character before the cursor
     }
 
@@ -143,7 +200,7 @@ public class SecureTextBox : TextBox
 
     if(e.KeyChar >= 32 && !char.IsControl(e.KeyChar)) // if it's not a control character...
     {
-      if(SelectionLength != 0) DeleteSelection(); // replace the selection if there is one
+      if(SelectionLength != 0) DeleteSelection(false); // replace the selection if there is one
       text.InsertAt(SelectionStart, e.KeyChar); // then insert at the cursor position
 
       e.KeyChar = '*'; // don't send the real characters to the base class
@@ -152,10 +209,30 @@ public class SecureTextBox : TextBox
     base.OnKeyPress(e);
   }
 
-  /// <summary>Removes all characters in the selection.</summary>
-  void DeleteSelection()
+  protected override void WndProc(ref Message m)
   {
-    for(int i=0; i<SelectionLength; i++) text.RemoveAt(SelectionStart);
+    // we need to intercept clipboard commands to avoid the textbox getting out of sync with the SecureString
+    switch(m.Msg)
+    {
+      case 0x0300: DeleteSelection(true); break; // WM_CUT
+      case 0x0302: Paste(); break; // WM_PASTE
+      case 0x0303: Clear(); break; // WM_CLEAR
+      case 0x0304: // WM_UNDO
+        m.Result = new IntPtr(0); // we don't support undo
+        return;
+      default:
+        base.WndProc(ref m);
+        return;
+    }
+
+    m.Result = new IntPtr(1);
+  }
+
+  /// <summary>Removes all characters in the selection.</summary>
+  void DeleteSelection(bool includeBaseText)
+  {
+    for(int i=0, end=Math.Min(SelectionLength, text.Length-SelectionStart); i<end; i++) text.RemoveAt(SelectionStart);
+    if(includeBaseText) base.Text = base.Text.Remove(SelectionStart, SelectionLength);
   }
 
   /// <summary>Determines whether the character is outside the allowable range of low ASCII characters.</summary>
